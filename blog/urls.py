ab9ed01bf49db1fbc17cf8077858ca904e3486d0
Mario <krajacic.mario@gmail.com>
2019-07-13 06:18:13 +0200

    Removing blog app

--- blog/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.blog_home, name='blog-home'),
-    path('about/', views.blog_about, name='blog-about'),
-]
--- mario_krajacic_com_landing_page/urls.py
+++ mario_krajacic_com_landing_page/urls.py
@@ -20,5 +20,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('homepage.urls')),
-    path('blog/', include('blog.urls')),
 ]
