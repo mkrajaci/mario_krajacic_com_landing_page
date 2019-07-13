5803ae1de662e78724b066c54100f38c18144019
Mario <krajacic.mario@gmail.com>
2019-07-13 07:22:23 +0200

    Adding Bootstrap and web pages

--- homepage/urls.py
+++ homepage/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='homepage-home'),
     path('about/', views.about, name='homepage-about'),
+    path('blog/', views.blog, name='homepage-blog'),
+    path('projects/', views.projects, name='homepage-projects'),
 ]
--- homepage/views.py
+++ homepage/views.py
@@ -1,11 +1,37 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        'author': 'Mario',
+        'title': 'Blog post 1',
+        'content': 'First content',
+        'date_posted': 'July 12, 2019',
+    },
+    {
+        'author': 'Marija',
+        'title': 'Blog post 2',
+        'content': 'Second content',
+        'date_posted': 'July 13, 2019',
+    }
+]
+
 
 # Create your views here.
 def home(request):
-    return HttpResponse('<h1>Homepage home</h1>')
+    return render(request, 'homepage/home.html')
 
 
 def about(request):
-    return HttpResponse('<h1>Homepage about</h1>')
+    return render(request, 'homepage/about.html', {'title': 'About'})
+
+
+def blog(request):
+    context = {
+        'posts': posts
+    }
+    return render(request, 'homepage/blog.html', context)  # Kroz context šalješ na frontu varijable
+
+
+def projects(request):
+    return render(request, 'homepage/projects.html')
