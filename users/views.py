4c771d9e2c7327d3ace2e451503aced7c70625d9
Mario <krajacic.mario@gmail.com>
2019-07-16 06:04:55 +0200

    Adding login and logout pages and logic

--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm
 
 # Create your views here.
@@ -20,8 +21,13 @@ def register(request):
             messages.warning
             messages.error
             '''
-            messages.success(request, f'Account created for {username}!')
-            return redirect('homepage-blog')
+            messages.success(request, f'Your account has been created! You are now able to log in.')
+            return redirect('login')
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
+
+
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
