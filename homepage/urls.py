0a5375ce766ddce7de8457225f3f7407af6f1a4d
Mario <krajacic.mario@gmail.com>
2019-07-18 06:01:26 +0200

    Implementing PostListView and PostDetailView

--- homepage/urls.py
+++ homepage/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
+from .views import PostListView, PostDetailView
 from . import views
 
 urlpatterns = [
     path('', views.home, name='homepage-home'),
     path('about/', views.about, name='homepage-about'),
-    path('blog/', views.blog, name='homepage-blog'),
+    path('blog/', PostListView.as_view(), name='homepage-blog'),
+    # int:pk ogranicavam na integer, u slucaju da dodje string
+    path('blog/post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('projects/', views.projects, name='homepage-projects'),
 ]
--- homepage/views.py
+++ homepage/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from .models import Post
 from django.http import HttpResponse
+
+
+class PostListView(ListView):
+    model = Post
+    # PostListView trazi template_name u ovom formatu
+    # <app>/<model>_<viewtype>.html
+    # homepage/post_list.html
+    template_name = 'homepage/blog.html'
+    context_object_name = 'posts'
+    # Promjena redosljeda postova
+    ordering = ['-date_posted']
+
+
+class PostDetailView(DetailView):
+    model = Post
 
 
 # Create your views here.
