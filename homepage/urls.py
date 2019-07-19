c5b1e23cc6d7e0c88186745ec7644bc2fedc8adf
Mario <krajacic.mario@gmail.com>
2019-07-19 05:50:13 +0200

    Implementing Class based views

--- homepage/urls.py
+++ homepage/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import PostListView, PostDetailView
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
 from . import views
 
 urlpatterns = [
@@ -8,5 +8,8 @@ urlpatterns = [
     path('blog/', PostListView.as_view(), name='homepage-blog'),
     # int:pk ogranicavam na integer, u slucaju da dodje string
     path('blog/post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('blog/post/new/', PostCreateView.as_view(), name='post-create'),
+    path('blog/post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('blog/post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
     path('projects/', views.projects, name='homepage-projects'),
 ]
--- homepage/views.py
+++ homepage/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
-from django.http import HttpResponse
 
 
+# Pogledaj 10 video tutoriala za objasnjenja ovih class based viewa
+# https://youtu.be/-s7e_Fy6NRU
 class PostListView(ListView):
     model = Post
     # PostListView trazi template_name u ovom formatu
@@ -17,6 +19,47 @@ class PostListView(ListView):
 
 class PostDetailView(DetailView):
     model = Post
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    # Presretanje form_valid metode i njena nadogradnja
+    # Postavljanje autora posta na trenutno logiranog korisnika
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+# Ovi Mixini sluze za provjeru je li korisnik logiran i da ne smije editirati
+# postove drugog korisnika
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+
+    # Presretanje form_valid metode i njena nadogradnja
+    # Postavljanje autora posta na trenutno logiranog korisnika
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = '/blog'
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
 
 
 # Create your views here.
