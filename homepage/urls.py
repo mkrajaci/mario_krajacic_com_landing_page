be03ec0972b7b23013802a75dd231a38220e9720
Mario <krajacic.mario@gmail.com>
2019-07-20 08:19:02 +0200

    Implementing pagination by user

--- homepage/urls.py
+++ homepage/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView, UserPostListView
 from . import views
 
 urlpatterns = [
     path('', views.home, name='homepage-home'),
     path('about/', views.about, name='homepage-about'),
     path('blog/', PostListView.as_view(), name='homepage-blog'),
+    path('blog/user/<str:username>/', UserPostListView.as_view(), name='user-posts'),
     # int:pk ogranicavam na integer, u slucaju da dodje string
     path('blog/post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('blog/post/new/', PostCreateView.as_view(), name='post-create'),
