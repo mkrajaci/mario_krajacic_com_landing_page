a848c0790e5a6cc2bdf1af12cb450d181ee36418
Mario <krajacic.mario@gmail.com>
2019-07-20 07:56:43 +0200

    Implementing pagination

--- homepage/views.py
+++ homepage/views.py
@@ -15,6 +15,7 @@ class PostListView(ListView):
     context_object_name = 'posts'
     # Promjena redosljeda postova
     ordering = ['-date_posted']
+    paginate_by = 5
 
 
 class PostDetailView(DetailView):
